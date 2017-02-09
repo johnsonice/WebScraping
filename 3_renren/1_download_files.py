dba6a276990baf155c4891044b5f59092d90004c
Chengyu Huang <chengyuhuang16@gmail.com>
2017-02-08 21:53:44 -0500

    download

--- 3_renren/1_download_files.py
+++ 3_renren/1_download_files.py
@@ -6,18 +6,22 @@ Created on Mon Feb  6 09:29:36 2017
 @author: huang
 """
 
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import os
-import re
+from functools import partial
+#import re
 from multiprocessing import Pool 
 
 os.getcwd()
 os.chdir('/Users/huang/Dev/projects/WebScraping/3_renren/')
 #%%
 ## download 
-def download_zip(url,folder,fname):
+def download_zip(f,folder):
+    index,row = f 
+    url = row['links']
+    fname = row['name']
     try:
         result = requests.get(url)
         res= result.content
@@ -25,18 +29,40 @@ def download_zip(url,folder,fname):
         #fname = result.headers['Content-Disposition'].split('"')[1]    
         with open( folder + fname, "wb" ) as f :
                 f.write( res )
+        if index % 50 == 0 :
+            print(index)
+            
         return True
     except:
         return False
+
+
     
 #%%
-data_folder = 'data/'
-download_folder = data_folder + 'download/'
-if not os.path.exists(download_folder):
-    os.makedirs(download_folder)
-
-df = pd.read_csv('links.csv')
-
-
+if __name__ == '__main__':
+    data_folder = 'data/'
+    download_folder = data_folder + 'download/'
+    if not os.path.exists(download_folder):
+        os.makedirs(download_folder)
+    ## read download links from previous csv file 
+    df = pd.read_csv('links.csv')
+    ## download files from renren 
+    ## multi process 
+    p = Pool(20)
+    partial_download = partial(download_zip, folder = download_folder)
+    all_links_mp = p.map(partial_download,df.iterrows())
+    p.close()
+    p.join()
+    print('finish')
     
+    ## single process 
+#==============================================================================
+#for x in df.iterrows():
+#    if x[0]> 10:
+#        break
+#    print(download_zip(x,download_folder))
+#==============================================================================
+#%%
+
+
 
